d29f87cf34dca265a18fcd939c8aaf4917db0ada
mega05 <benape05@gmail.com>
2026-05-16 17:11:40 -0700

    v0.5.2

--- cogs/stats.py
+++ cogs/stats.py
@@ -64,12 +64,12 @@ class stats(commands.Cog):
                 member_vals.append((member, val))
         
         for i in range(len(member_vals)):
+            if i > 20:
+                if author_data != None: break
+                continue
             member, val = member_vals[i]
             top20 += f"{i + 1}. {member.display_name} - **{val}**\n"
             if member.id == author.id: author_data = f"-# Your Positon: {ordinal(i + 1)} (score: {val})."
-            if i > 19:
-                if author_data != None: break
-                continue
         
         if author_data != None: top20 += author_data
         embed = discord.Embed(
